e5da95d65c75c599b46b6e98c0e4d0f721b93523
Usman Hamid <55622390+AWESOMEPIEMAN@users.noreply.github.com>
2021-03-12 07:11:17 +0500

    Add files via upload

---
+++ source.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+
+    int a = 0, b - 0;
+
+    int c = 0;
+
+    c = a + b;
+
+    cout<<"Input value 1 : ";
+    cin>>a;
+    cout<<"Input value 2 : ";
+    cin>>b;
+
+    cout<< "The outout is : " << c;
+}
